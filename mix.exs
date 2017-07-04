ca6a5f7e6822f623b4f13e7a1f6345212a68b9ec
Shane Kilkelly <shane@kilkelly.me>
2017-07-04 21:50:47 +0100

    Add mocking lib

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule HammerBackendRedis.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:redix, "~> 0.6"}]
+    [{:redix, "~> 0.6"},
+     {:mock, "~> 0.2.0", only: :test}]
   end
 end
