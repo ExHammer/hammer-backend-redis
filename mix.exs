9d0f2f390f2710bf28794fd8832bc4fba19eb68f
Shane Kilkelly <shane@kilkelly.me>
2017-09-20 18:56:07 +0100

    Remove `redix` from extra_applications

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule HammerBackendRedis.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :redix]]
+    [extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
